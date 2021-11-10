a8bfb4253cc07aec0cefeca0aea62dd27fa02bb9
JOANITA-51 <nakityojoanita51@gmail.com>
2021-11-10 11:13:23 +0300

    connecting to the database

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const bodyParser = require ("body-parser")
 //configure what we have created
 app.use(bodyParser.urlencoded({extended:true}));
 
+//connecting to the database
+mangoose.connect("mongodb+srv://JOANITA:WoebXeNVeVEzZ8DL@ablestatecohort1.hx7bi.mongodb.net/Forms")
+
 //checking our express server
 app.get("/", function(req, res){
     res.send ("express is working")
