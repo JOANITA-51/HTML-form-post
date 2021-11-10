5ac2fb99300626ac5c62054f1e5f2ff907017ae5
JOANITA-51 <nakityojoanita51@gmail.com>
2021-11-10 12:33:26 +0300

    posting info from server to mango DB

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require ("express")
 const app = express ()
 const mongoose = require ("mongoose")
-const bodyParser = require ("body-parser")
+const bodyParser = require ("body-parser");
+const { request } = require("http");
 
 //configure what we have created
 app.use(bodyParser.urlencoded({extended:true}));
 
 //connecting to the database
-mongoose.connect("mongodb+srv://JOANITA:WoebXeNVeVEzZ8DL@ablestatecohort1.hx7bi.mongodb.net/Forms")
+mongoose.connect("mongodb+srv://JOANITA:WoebXeNVeVEzZ8DL@ablestatecohort1.hx7bi.mongodb.net/notesDB")
 
 //creating a data schema :an abstract design that represents the storage of the data in a database.
 const notesSchema = {
@@ -18,9 +19,20 @@ const notesSchema = {
 //creating a model :abstract model that organizes elements of data
 const note = mongoose.model("Note", notesSchema)
 
-//checking our express server
+//getting from our express server
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/index.html");
+})
+
+//taking in the data from the form that has been referenced by the names
+app.post ("/", function(req, res){
+    let newNote = new note ({
+        title: req.body.title,
+        content:req.body.content
+    });
+    newNote.save();
+    //to prevent the over loading 
+    res.redirect("/")
 })
 
 app.listen(3000, function(){
